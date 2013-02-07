d268f07e082bf5c29d40e8ba517b27532098b1c0
Freehan <freehan@cs.ucla.edu>
2013-02-07 13:27:48 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Author:	Minhan Xia
+Date:	2013-1-25
+Project:Scalable and Reliable Multicast for File Distribution
---
+++ Test/FileCombination/src/FileCombination.java
@@ -0,0 +1,12 @@
+
+public class FileCombination {
+	
+	String srcDirectory = null;
+	String[] separatedFiles;
+	String[][] separatedFilesAndSize;
+	int fileNum=0;
+	String fileReadName="";
+	
+	
+	public FileCombination(){};
+}
